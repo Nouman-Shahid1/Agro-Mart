8d870fc71ea3c9927fc1c9340737f2af5094ad4d
H-Khichi <145919359+H-Khichi@users.noreply.github.com>
2024-11-22 17:06:36 +0500

    ui of product-catalog and products

--- web/src/app/product-catalog/page.jsx
+++ web/src/app/product-catalog/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Newsletter from "@/components/Newsletter";
+import ProductCard from "@/components/ProductCard";
 import { useState } from "react";
 
-const ProductCatalog = () => {
+const ProductCatalog = ({ bground }) => {
   const initialProducts = [
     {
       id: 1,
@@ -83,6 +85,55 @@ const ProductCatalog = () => {
       available: true,
       category: "Seeds",
     },
+    {
+      id: 9,
+      name: "Seeds",
+      imageUrl: "https://i.brecorder.com/large/2024/03/6604aab2bf8c4.jpg",
+      price: 25,
+      rating: 4,
+      available: true,
+      category: "Seeds",
+    },
+    {
+      id: 10,
+      name: "Fertilizer",
+      imageUrl:
+        "https://image.made-in-china.com/202f0j00zlprGMCoYkqP/Herbicide-Bensulfuron-Methyl-10-Bentazole-45-Wp-Hot-Sales-Keep-Grass-Products.webp",
+      price: 20,
+      rating: 4,
+      available: true,
+      category: "Fertilizer",
+    },
+    {
+      id:11,
+      name: "Pesticide",
+      imageUrl:
+        "https://image.made-in-china.com/2f0j00RfLqdFvkCgpV/Best-Selling-Agriculture-Pesticides-Emamectin-Benzoate-Abamectin-Insektisida-Abamectin-95-Tc-1-8c.webp",
+      price: 50,
+      rating: 5,
+      available: false,
+      category: "Pesticide",
+    },
+    {
+      id: 12,
+      name: "Farm Machinery",
+      imageUrl:
+        "https://image.made-in-china.com/2f0j00gvHqFsQlEhbe/Agricultural-Machine-and-Farm-Equipment-Rice-Wheat-Combine-Harvester.webp",
+      price: 30,
+      rating: 3,
+      available: true,
+      category: "Farm Machinery",
+    },
+    {
+      id: 13,
+      name: "Seeds",
+      imageUrl: "https://i.brecorder.com/large/2024/03/6604aab2bf8c4.jpg",
+      price: 25,
+      rating: 4,
+      available: true,
+      category: "Seeds",
+    },
+  
   ];
 
   const categories = [
@@ -128,83 +179,74 @@ const ProductCatalog = () => {
 
   return (
     <div>
-      <Navbar />
-      <div className="p-6 pt-40 font-sans bg-white text-gray-800">
-        <h1 className="text-4xl font-bold mb-4 text-gray-800">
-          Product Catalog
-        </h1>
+      <Navbar bground={true} />
+      <div className="bg-gray-100">
+        <div className="p-6 pt-6 w-[80%] mx-auto font-sans bg-white text-gray-800">
+          <h1 className="text-4xl font-bold mb-4 text-gray-800">
+            Product Catalog
+          </h1>
 
-        <div className="flex items-center gap-4 mb-6">
-          <div className="flex items-center space-x-2">
-            <span className="text-lg font-semibold text-gray-800">
-              Your Shop
-            </span>
-          </div>
-
-          <input
-            type="text"
-            placeholder="Search products..."
-            onChange={(e) => handleSearch(e.target.value)}
-            className="flex-1 p-2 border border-gray-300 rounded-md focus:ring focus:ring-gray-400 bg-light-gray"
-          />
-
-          <div>
-            <h3 className="text-sm font-semibold mb-1 text-gray-800">
-              Category
-            </h3>
-            <select
-              onChange={(e) => handleFilter(e.target.value)}
-              className="p-2 border border-gray-300 rounded-md bg-light-gray text-gray-800"
-            >
-              {categories.map((category, index) => (
-                <option key={index} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div>
-            <h3 className="text-sm font-semibold mb-1 text-gray-800">
-              Sort By
-            </h3>
-            <select
-              onChange={(e) => handleSort(e.target.value)}
-              className="p-2 border border-gray-300 rounded-md bg-light-gray text-gray-800"
-            >
-              <option value="price-asc">Price: Low to High</option>
-              <option value="price-desc">Price: High to Low</option>
-              <option value="newest">Newest</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {filteredProducts.map((product) => (
-            <div
-              key={product.id}
-              className="border border-gray-300 rounded-md p-4 text-center shadow-sm bg-light-gray"
-            >
-              <img
-                src={product.imageUrl}
-                alt={product.name}
-                className="w-full h-40 object-cover mb-4 rounded-md"
+          <div class="sm:p-6 bg-gray-50 flex flex-col md:flex-row shadow-lg rounded-lg w-full justify-between my-6  mx-auto space-y-4">
+          
+            <div class="flex  items-center mt-8 md:ml-3 w-[100%] md:w-[400px] lg:w-[500px]" >
+              <input
+                type="text"
+                placeholder="Search..."
+                class="w-full p-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-green-500"
               />
-              <h4 className="font-semibold text-gray-800">{product.name}</h4>
-              <p className="text-gray-700">Price: ${product.price}</p>
-              <p className="text-yellow-500">Rating: {product.rating} ⭐</p>
-              <p
-                className={`font-semibold ${
-                  product.available ? "text-green-500" : "text-red-500"
-                }`}
+              <button
+                class="bg-green-700 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600"
               >
-                {product.available ? "In Stock" : "Out of Stock"}
-              </p>
+                Search
+              </button>
             </div>
-          ))}
+            <div className="flex flex-col md:flex-row gap-4">
+              
+              <div>
+                <h3 class="text-lg font-semibold mb-2">Categories</h3>
+                <select
+                onChange={(e) => handleFilter(e.target.value)}
+                  class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                >
+                  {categories.map((category, index) => (
+                    <option key={index} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div>
+                <h3 class="text-lg font-semibold mb-2">Sort by Price</h3>
+                <select
+                onChange={(e) => handleSort(e.target.value)}
+                  class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                >
+                  <option value="price-asc">Price: Low to High</option>
+                  <option value="price-desc">Price: High to Low</option>
+                  <option value="newest">Newest</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap justify-center gap-6">
+            {filteredProducts.map((product) => (
+              <ProductCard
+                title={product.name}
+                src={product.imageUrl}
+                price={product.price}
+                rating={product.rating}
+                avai={product.available}
+                cat={product.category}
+              />
+
+            ))}
+          </div>
         </div>
+        <Newsletter />
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { ImCross } from "react-icons/im";
-const Navbar = () => {
+const Navbar = ({bground}) => {
     const [bg, setBg] = useState(false);
     const [showNav, setShowNav] = useState(false);
 
@@ -23,7 +23,8 @@ const Navbar = () => {
     }
 
     return (
-        <div className={`${bg ? 'bg-white shadow-lg' : 'bg-transparent'} w-full  transition-colors duration-300 fixed  `}>
+        <>
+        <div className={`${bg||bground ? 'bg-white shadow-lg' : 'bg-transparent'} w-full  transition-colors duration-300 fixed z-10 `}>
             <div className='mx-auto w-[80%] relative'>
 
                 <div className="flex flex-col md:flex-row gap-5 md:gap-0 justify-between items-center py-4">
@@ -32,34 +33,34 @@ const Navbar = () => {
                             src="/logo.png"
                             alt="Logo"
                             width="170px"
-                            className={`transition-all duration-300 ease-in-out drop-shadow-md ${bg ? 'hover:drop-shadow-[0_4px_20px_rgba(76,175,80,1)] ' : 'hover:drop-shadow-[0px_4px_20px_rgba(255,255,255,1)]'} rounded-lg"`}
+                            className={`transition-all duration-300 ease-in-out drop-shadow-md ${bg||bground ? 'hover:drop-shadow-[0_4px_20px_rgba(76,175,80,1)] ' : 'hover:drop-shadow-[0px_4px_20px_rgba(255,255,255,1)]'} rounded-lg"`}
                         />
                     </div>
                     <div className="flex  gap-4 text-md pt-0 md:pt-4 ">
-                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full text-md  py-2 px-3 ${bg ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
+                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full text-md  py-2 px-3 ${bg||bground ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
                             Become a Buyer
                         </Link>
-                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full py-2 px-3 ${bg ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
+                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full py-2 px-3 ${bg||bground ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
                             Become a Seller
                         </Link>
-                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full py-2 px-3 ${bg ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
+                        <Link href='/' className={`bg-[#1fb025] text-white rounded-full py-2 px-3 ${bg||bground ? 'hover:border-2 border-[#017d29] hover:bg-white hover:text-[#017d29]' : ' hover:bg-white hover:text-[#017d29]'}`}>
                             Rent a Machine
                         </Link>
                     </div>
                     <div className="md:hidden absolute top-[50px] right-0  p-2" onClick={handleNavbar}>
-                        <span className={`block w-[30px] h-1 ${bg ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
-                        <span className={`block w-[30px] h-1 ${bg ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
-                        <span className={`block w-[30px] h-1 ${bg ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
+                        <span className={`block w-[30px] h-1 ${bg||bground ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
+                        <span className={`block w-[30px] h-1 ${bg||bground ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
+                        <span className={`block w-[30px] h-1 ${bg||bground ? 'bg-black' : 'bg-white'} mb-1 rounded-full`}></span>
                     </div>
 
 
 
                 </div>
-                <div className={`${showNav ? 'fixed left-0 top-0 bg-white w-full sm:w-[90%] h-screen py-8 px-3 ' : 'relative md:flex flex-col md:flex-row  py-1 '} ${bg?'border-t md:border-black':'border-t md:border-white'}`} >
+                <div className={`${showNav ? 'fixed left-0 top-0 bg-white w-full sm:w-[90%] h-screen py-8 px-3 ' : 'relative md:flex flex-col md:flex-row  py-1 '} ${bg||bground?'border-t md:border-black':'border-t md:border-white'}`} >
                     <div className={`${showNav?'block absolute right-[30px] top-[30px]':'hidden'}`} onClick={handleNavbar}>
                         <ImCross  style={{ color:'#017d29',fontSize:'20px' }}/>
                     </div>
-                    <ul className={`${showNav ? 'block' : 'hidden'} w-full md:flex  flex-col md:flex-row justify-around  mx-auto ${bg ? 'text-black gap-10 ' : 'text-[#017d29] md:text-white '}`}>
+                    <ul className={`${showNav ? 'block' : 'hidden'} w-full md:flex  flex-col md:flex-row justify-around  mx-auto ${bg||bground ? 'text-black gap-10 ' : 'text-[#017d29] md:text-white '}`}>
                         <Link href="/"><li className="hover:border-b-2 border-current p-2"><strong>HOME</strong></li></Link>
                         <Link href="/products"><li className="hover:border-b-2 border-current p-2"><strong>PRODUCTS</strong></li></Link>
                         <Link href="/seeds"><li className="hover:border-b-2 border-current p-2"><strong>SEEDS</strong></li></Link>
@@ -70,6 +71,8 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
+        <div className='pt-[150px]'></div>
+        </>
     );
 };
 
