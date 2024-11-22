b24de1800224140bfffbf9d9604873ec58c7069d
waheedanwar00 <waheedanwar322004@gmail.com>
2024-11-22 12:44:28 -0800

    update code

---
+++ web/src/app/aboutUs/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et im ad minim veniam quis
+            nostrud exercit atiooris.
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
