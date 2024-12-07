92c732f9d84dc241f76cd3d4867aae9bef5dd95e
waheedanwar00 <waheedanwar322004@gmail.com>
2024-12-07 09:29:49 -0800

    commit

--- web/src/app/rentalDetails/page.jsx
+++ web/src/app/rentalDetails/page.jsx
@@ -1,7 +1,20 @@
+import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer/Footer";
+import Newsletter from "@/Components/NewsLetter/Newsletter";
 import React from "react";
 
 const RentalDetails = () => {
-  return <div>RentalDetails</div>;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div>yeh</div>
+      </div>
+      ;
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
 
 export default RentalDetails;
