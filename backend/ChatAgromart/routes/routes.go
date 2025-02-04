e9f12d5f3b84f89084e059f146c878e718316558
Kiraid <faizan.akhtar3130@gmail.com>
2025-02-04 17:30:12 +0500

    commit

---
+++ backend/ChatAgromart/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"example.com/chat/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	authenticated := server.Group("/message")
+	authenticated.Use(middleware.Authenticate)
+	authenticated.POST("/new", NewMessage)
+	authenticated.GET("messages", GetMessages)
+
+}
